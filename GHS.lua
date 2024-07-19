56934f8ef229323df945bfc68c78a726cb5d87cd
c0-a1 <171464559+c0-a1@users.noreply.github.com>
2024-07-19 00:39:54 -0700

    Update and rename GHS2.lua to GHS.lua

---
+++ GHS.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/c0-a1/raw/main/GHS.lua'))()
--- GHS2.lua
+++
@@ -1,1 +0,0 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/c0-a1/Scripts/main/GHS.lua'))()
